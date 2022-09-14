19e3d10c3049948a74d53cf21d24c57ffb830d2d
kusasasasa <ec2-user@ip-172-31-21-253.ap-northeast-1.compute.internal>
2022-09-14 10:41:37 +0000

    [Finish]課題2：アプリケーションを作成してみよう：基礎編

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,29 +2,48 @@ class BooksController < ApplicationController
   def new
   end
   def index
-    @book=Book.new
 
+    @books=Book.all
+    @book=Book.new
 
   end
 
   def create
     @book=Book.new(book_params)
-    
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    if @book.save!
-    # 4. トップ画面へリダイレクト
-      @book.save
-      
-      redirect_to book_path(@book)
+    if @book.save
+      flash[:complete] ="Book was successfully created."
+      redirect_to book_path(@book.id)
+
+    else
+      @books =Book.all
+      render :index
     end
   end
   def show
-    
-    @books=Book.find(params[:id])
+
+    @book=Book.find(params[:id])
 
   end
   def edit
 
+    @book=Book.find(params[:id])
+    
+
+  end
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:complete] ="Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      
+      render :edit
+    end
+  end
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to books_path
   end
   private
   # ストロングパラメータ
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  get 'books/new'
-  get 'books/index'
-  get 'books/edit'
-  get 'books/:id' => 'books#show'
+  resources:books
+  
+  
 
   root :to => 'homes#new'
-  resources :books
 
 end
 
